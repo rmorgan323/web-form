f83b04a489cc84d186a76583d560f89f86b8c29e
Rob Morgan <rmorgan323@gmail.com>
2018-04-17 12:04:17 -0600

    initial commit

---
+++ cypress/integration/hhbuilder_spec.js
@@ -0,0 +1,136 @@
+describe('HHBuilder end-to-end tests', function () {
+  beforeEach(function () {
+    cy.visit('/index.html');
+  });
+
+  it('should contain specific elements', function () {
+    cy.title().should('include', 'Household builder');
+    cy.get('input[name="age"]').should('exist');
+    cy.get('select[name="rel"]').should('exist');
+    cy.get('input[name="smoker"]').should('exist');
+    cy.get('.add').should('exist');
+    cy.get('button[type="submit"]').should('exist');
+    cy.get('.debug').should('exist');
+  });
+
+  it('should have default inputs', function () {
+    cy.get('input[name="age"]').should('be.empty');
+    cy.get('select[name="rel"]').should('have.value', '');
+    cy.get('input[name="smoker"]').should('not.be.checked');
+  });
+
+  it('should require values for age and relationship', function () {
+    cy.get('.add').click();
+    cy.get('.age-msg').should('have.text', '* Required');
+    cy.get('.rel-msg').should('have.text', '* Required');
+  });
+
+  it('error messages should disappear when errors are corrected', function () {
+    cy.get('.add').click();
+    cy.get('.age-msg').should('exist');
+    cy.get('.rel-msg').should('exist');
+
+    cy.get('input[name="age"]').type('12');
+    cy.get('.age-msg').should('not.exist');
+    cy.get('.rel-msg').should('exist');
+
+    cy.get('select[name="rel"]').select('Child');
+    cy.get('.rel-msg').should('not.exist');
+  });
+
+  it('should require specific values in age input', function () {
+    cy.get('input[name="age"]').type('abcd');
+    cy.get('.age-msg').should('have.text', '* Please input a valid age');
+  });
+
+  it('should require values > 0 for age input', function () {
+    cy.get('input[name="age"]').type('0');
+    cy.get('.age-msg').should('have.text', '* Age should be between 1 - 120');
+  });
+
+  it('should require values <= 120 for age input', function () {
+    cy.get('input[name="age"]').type('121');
+    cy.get('.age-msg').should('have.text', '* Age should be between 1 - 120');
+  });
+
+  it('should allow spaces in age input', function () {
+    cy.get('input[name="age"]').type('17   ');
+    cy.get('.age-msg').should('not.exist');
+  });
+
+  it('should display member when user clicks add button (with valid inputs)', function () {
+    cy.get('.member-profile').should('not.exist');
+
+    cy.get('input[name="age"]').type('12');
+    cy.get('select[name="rel"]').select('Child');
+    cy.get('.add').click();
+
+    cy.get('.member-profile > h4').should('have.text', 'child');
+    cy.get('.member-profile > p').eq(0).should('have.text', 'Age: 12');
+    cy.get('.member-profile > p').eq(1).should('have.text', 'Non-smoker');
+  });
+
+  it('should be able to add multiple members', function () {
+    cy.get('input[name="age"]').type('12');
+    cy.get('select[name="rel"]').select('Child');
+    cy.get('.add').click();
+
+    cy.get('input[name="age"]').type('47');
+    cy.get('select[name="rel"]').select('Parent');
+    cy.get('input[name="smoker"]').check();
+    cy.get('.add').click();
+
+    cy.get('.member-profile').eq(0).should('exist');
+    cy.get('.member-profile').eq(1).should('exist');
+  });
+
+  it('should clear all inputs when user successfully adds a member', function () {
+    cy.get('input[name="age"]').type('47');
+    cy.get('select[name="rel"]').select('Parent');
+    cy.get('input[name="smoker"]').check();
+    cy.get('.add').click();
+
+    cy.get('input[name="age"]').should('be.empty');
+    cy.get('select[name="rel"]').should('have.value', '');
+    cy.get('input[name="smoker"]').should('not.be.checked');
+  });
+
+  it('should be able to delete a member', function () {
+    cy.get('input[name="age"]').type('81');
+    cy.get('select[name="rel"]').select('Grandparent');
+    cy.get('.add').click();
+
+    cy.get('.member-profile').should('exist');
+    cy.get('.delete-member').should('exist');
+
+    cy.get('.delete-member').click();
+
+    cy.get('.member-profile').should('not.exist');
+    cy.get('.delete-member').should('not.exist');
+  });
+
+  it('submit button should be enabled only when members are present', function () {
+    cy.get('button[type="submit"]').should('be.disabled');
+
+    cy.get('input[name="age"]').type('81');
+    cy.get('select[name="rel"]').select('Grandparent');
+    cy.get('.add').click();
+
+    cy.get('button[type="submit"]').should('be.enabled');
+
+    cy.get('.delete-member').click();
+
+    cy.get('button[type="submit"]').should('be.disabled');
+  });
+
+  it('should append json household data to .debug upon submission', function () {
+    var expected = '{"0":{"age":"30","relationship":"self","smoker":"false"}}';
+
+    cy.get('input[name="age"]').type('30');
+    cy.get('select[name="rel"]').select('Self');
+    cy.get('.add').click();
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.debug').should('have.text', expected);
+  });
+});
---
+++ index.js
@@ -0,0 +1,170 @@
+function validateInputs() {
+  var age = validateAge();
+  var rel = validateRel();
+  return { age: age, rel: rel };
+}
+
+function validateRel() {
+  if (document.querySelector('select[name="rel"]').value.length) {
+    removeError('rel');
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateAge() {
+  var onlyNumbersRegex = /^[0-9]+$/;
+  var spacesRegex = /\s/g;
+  var userInput = document.querySelector('input[name="age"]').value.replace(spacesRegex, '');
+
+  if (userInput === '') {
+    removeError('age');
+    return false;
+  } else if (userInput.match(onlyNumbersRegex) 
+      && userInput > 0 
+      && userInput <= 120 
+      || userInput === '') {
+    removeError('age');
+    return true;
+  } else if (!userInput.match(onlyNumbersRegex)) {
+    displayError('notANumber', 'age');
+    return false;
+  } else {
+    displayError('outOfRange', 'age');
+    return false;
+  }
+}
+
+function submitForm() {
+  var allMemberInfo = getMemberInfo();
+  var serializedData = JSON.stringify(allMemberInfo);
+  displaySerializedData(serializedData);
+}
+
+function addMember() {
+  var inputs = validateInputs();
+
+  if (inputs.age && inputs.rel) {
+    var spacesRegex = /\s/g;
+    var memAge = document.querySelector('input[name="age"]').value.replace(spacesRegex, '');
+    var memRel = document.querySelector('select[name="rel"]').value;
+    var memSmk = document.querySelector('input[name="smoker"]').checked;
+
+    displayMember(memAge, memRel, memSmk);
+    clearInputs();
+    enableSubmit();
+  } else {
+    if (!inputs.age) displayError('required', 'age');
+    if (!inputs.rel) displayError('required', 'rel');
+  }
+}
+
+function clearInputs() {
+  document.querySelector('input[name="age"]').value = '';
+  document.querySelector('select[name="rel"]').selectedIndex = 0;
+  document.querySelector('input[name="smoker"]').checked = false;
+}
+
+function enableSubmit() {
+  document.querySelector('button[type="submit"]').disabled = false;
+}
+
+function disableSubmit() {
+  document.querySelector('button[type="submit"]').disabled = true;
+}
+
+function removeError(loc) {
+  var curr = document.querySelector('.' + loc + '-msg');
+  if (curr) curr.remove();
+}
+
+function removeMember(id) {
+  document.getElementById(id).closest('article').remove();
+  if (!checkMembers()) disableSubmit();
+}
+
+function getMemberInfo() {
+  var members = [].slice.call(document.querySelectorAll('.member-profile'));
+
+  return members.reduce((accum, member, index) => {
+    var memberData = member.children[0].dataset;
+    accum[index] = {
+      age: memberData.age,
+      relationship: memberData.rel,
+      smoker: memberData.smoker
+    };
+    return accum;
+  }, {});
+}
+
+function checkMembers() {
+  return document.querySelectorAll('.member-profile').length;
+}
+
+function displayMember(age, rel, smk) {
+  var smokeStatus = smk ? 'Smoker' : 'Non-smoker';
+  var memberId = Date.now();
+  var newMem = document.createElement('article');
+
+  newMem.setAttribute('style', 'background: #eee;');
+  newMem.classList.add('member-profile');
+  newMem.innerHTML = (
+    '<h4 data-age=' + age + ' data-rel=' + rel + ' data-smoker=' + smk + '>' + rel + '</h4>' +
+    '<p>Age: ' + age + '</p>' + 
+    '<p>' + smokeStatus + '</p>' + 
+    '<button class="delete-member" id="member-' + memberId + '">delete member</button>'
+  );
+
+  document.querySelector('.builder').appendChild(newMem);
+}
+
+function displayError(type, loc) {
+  var types = {
+    notANumber: '* Please input a valid age',
+    outOfRange: '* Age should be between 1 - 120',
+    required: '* Required'
+  };
+  var locs = {
+    age: { element: 'input', className: 'age-msg'},
+    rel: { element: 'select', className: 'rel-msg'}
+  };
+  var curr = document.querySelector('.' + locs[loc].className);
+
+  if (!curr) {
+    var newErr = document.createElement('span');
+
+    newErr.classList.add(locs[loc].className);
+    newErr.setAttribute('style', "color:red");
+    newErr.innerText = types[type];
+
+    document.querySelector(locs[loc].element + '[name=' + loc + ']')
+      .closest('div').appendChild(newErr);
+  }
+}
+
+function displaySerializedData(jsonData) {
+  var display = document.createElement('p');
+  display.innerText = jsonData;
+  document.querySelector('.debug').appendChild(display);
+}
+
+document.querySelector('input[name="age"]').addEventListener('keyup', validateAge, false);
+document.querySelector('select[name="rel"]').addEventListener('change', validateRel, false);
+document.querySelector('button[type="submit"]').addEventListener('click', function(event) {
+  event.preventDefault();
+  submitForm();
+}, false);
+document.querySelector('.add').addEventListener('click', function(event) {
+  event.preventDefault();
+  addMember();
+}, false);
+document.querySelector('.builder').addEventListener('click', function(event) {
+  if (event.target.classList.contains('delete-member')) {
+    event.preventDefault();
+    removeMember(event.target.id);
+  }
+}, false);
+document.addEventListener('DOMContentLoaded', function() {
+  disableSubmit();
+});
